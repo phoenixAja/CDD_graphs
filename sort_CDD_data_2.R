4573b5beb14563e44393fddfa6b3048add17b482
Phoenix Logan <plogan@wisc.edu>
2015-09-08 15:52:16 -0500

    added better data sorting technique for well names

--- sort_CDD_data_2.R
+++ sort_CDD_data_2.R
@@ -27,37 +27,38 @@ order_indexes <- function(df){
   }
 }
 
-ordered_indexes <- c("A2", "A3","A4", "A5","A6", "A7","A8", "A9","A10", "A11", "B11","B10","B9","B8","B7","B6","B5","B4", "B3", "B2", "C2", "C3", "C4","C5","C6","C7","C8","C9","C10","C11","D11","D10","D9","D8","D7","D6","D5","D4","D3","D2","E2","E3","E4","E5","E6","E7","E8","E9","E10","E11","F11","")
-
 well_idx_vector <- as.vector(sapply(synonyms, get_well_index))
+
+ind <- data.frame(well_idx_vector=paste0(rep(LETTERS[1:8], each=11), c(1:11, 11:1)), stringsAsFactors=FALSE)
 
 CA_neg <- data_table$CA.Dose.Response.Data....negative.control....
 CA_inv_neg <- as.vector(sapply(CA_neg, inv_neg))
 CA_data <- data.frame(well_idx_vector, mass=data_table$Mass..mg., volume=data_table$CA.Dose.Response.Data..Volume..nL., neg_control=CA_inv_neg, stringsAsFactors = FALSE)
 CA_data_500 <- which(CA_data$volume == 500)
 CA_DATA_parsed <- CA_data[CA_data_500,]
-
+CA_2 <- merge(ind, CA_DATA_parsed, by="well_idx_vector", sort=FALSE)
 
 EC_neg <- data_table$EC.Dose.Response.Data....negative.control....
 EC_inv_neg <- as.vector(sapply(EC_neg, inv_neg))
 EC_data <- data.frame(well_idx_vector, mass=data_table$Mass..mg., volume=data_table$EC.Dose.Response.Data..Volume..nL., neg_control=EC_inv_neg, stringsAsFactors = FALSE)
 EC_data_500 <- which(EC_data$volume == 500)
 EC_DATA_parsed <- EC_data[EC_data_500,]
-
-
+EC_2 <- merge(ind, EC_DATA_parsed, by="well_idx_vector", sort=FALSE)
 
 PA_neg <- data_table$PA.Dose.Response.Data....negative.control....
 PA_inv_neg <- as.vector(sapply(PA_neg, inv_neg))
 PA_data <- data.frame(well_idx_vector, mass=data_table$Mass..mg., volume=data_table$PA.Dose.Response.Data..Volume..nL., neg_control=PA_inv_neg, stringsAsFactors = FALSE)
 PA_data_500 <- which(PA_data$volume == 500)
 PA_DATA_parsed <- PA_data[PA_data_500,]
+PA_2 <- merge(ind, PA_DATA_parsed, by="well_idx_vector", sort=FALSE)
 
 SA_neg <- data_table$SA.Dose.Response.Data....negative.control....
 SA_inv_neg <- as.vector(sapply(SA_neg, inv_neg))
 SA_data <- data.frame(well_idx_vector, mass=data_table$Mass..mg., volume=data_table$SA.Dose.Response.Data..Volume..nL., neg_control=SA_inv_neg, stringsAsFactors = FALSE)
 SA_data_500 <- which(SA_data$volume == 500)
 SA_DATA_parsed <- SA_data[SA_data_500,]
+SA_2 <- merge(ind, SA_DATA_parsed, by="well_idx_vector", sort=FALSE)
 
 
+#Plot the 4 datasetts
 
-parcoord(data_table, col=rainbow(), lty=1, var.label = TRUE)
